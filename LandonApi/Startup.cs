2e0268586dedaadb809a2521c99fe61a65130378
Serhii Levytskyi <AUX502655@prrft.refinitiv.com>
2021-12-28 10:16:41 +0200

    Add project files.

---
+++ LandonApi/Startup.cs
@@ -0,0 +1,137 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using LandonApi.Filters;
+using LandonApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using LandonApi.Services;
+using LandonApi.Infrastructure;
+
+namespace LandonApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.Configure<HotelInfo>(Configuration.GetSection("Info"));
+
+            services.AddScoped<IRoomService, DefaultRoomService>();
+
+            services.AddDbContext<HotelApiDbContext>(options => {
+                var connectionString = Configuration.GetSection("ConnectionString").Value;
+                options.UseSqlServer(connectionString);
+            });
+            
+            /*
+            services.AddDbContext<HotelApiDbContext>(
+                options => options.UseInMemoryDatabase("landondb"));
+            */
+            services.AddMvc(options =>
+            {
+                options.Filters.Add<JsonExceptionFilter>();
+                options.Filters.Add<RequireHttpsOrCloseAttribute>();
+                options.EnableEndpointRouting = false;
+            });
+            //services.AddSwaggerDocument();
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "ToDo API";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = "https://twitter.com/spboyer"
+                    };
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    };
+                };
+            });
+
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader
+                    = new MediaTypeApiVersionReader();
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionSelector
+                     = new CurrentImplementationApiVersionSelector(options);
+            });
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowMyApp",
+                     policy => policy//.WithOrigins("https://example.com")
+                         .AllowAnyOrigin()); // 
+            });
+
+            services.AddAutoMapper( options => options.AddProfile<MappingProfile>());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
+
+                /*
+                app.UseSwaggerUi3WithApiExplorer(options =>
+                {
+                    options.GeneratorSettings
+                        .DefaultPropertyNameHandling
+                    = NJsonSchema.PropertyNameHandling.CamelCase;
+                });
+                */
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+
+            app.UseCors("AllowMyApp");
+
+            app.UseMvc();
+
+            app.UseRouting();
+
+            /*
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            */
+        }
+    }
+}
